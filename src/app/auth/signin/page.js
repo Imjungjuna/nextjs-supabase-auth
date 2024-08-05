333aadf8c1c9544310abe1541999b493002e55a4
Lim JungJun <134128463+Imjungjuna@users.noreply.github.com>
2024-08-05 17:48:35 +0900

    Update page.js

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -24,7 +24,7 @@ export default function SignIn() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">로그인</h1>
+        <h1 className="text-2xl font-bold mb-4">Comit에 통합 회원가입</h1>
         <button
           onClick={handleGoogleSignIn}
           className="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
